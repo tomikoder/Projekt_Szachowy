454bd09db7cfb60385cc4c6b5689011dbb3aa076
Tomek J <tommi_j@yahoo.com>
2022-02-09 20:40:55 +0100

    Commit2 Poprawiłem stylistycznie kod.

--- chess_page/chess_page/settings.py
+++ chess_page/chess_page/settings.py
@@ -11,13 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
-environ.Env.read_env('env.myvariables')
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
+SECRET_KEY = os.environ.get('DJANGO')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 'MoveCheckConfig'
